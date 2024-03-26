fdfdda0a2475db8d62e1457381db7f2b8e739c8d
dina4555 <ouazzanidina92@gmail.com>
2024-03-26 20:22:54 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        bambou: "url('/images/bambou4.jpg')",
+      },
+    },
+  },
+  plugins: [],
+};
